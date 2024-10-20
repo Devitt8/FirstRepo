b6cb9ae8f07764bf65ee2bed18dd10dea9047350
Devitt8 <itruhinmaxim@gmail.com>
2024-10-20 21:41:03 +0400

    Update hw.py
    cancel

--- hw.py
+++ hw.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 name = input ("Как вас зовут?")
 print ("Привет,", name)
-//comment
-=======
-full_name = input ("Как вас зовут?")
-print ("Привет,", full_name)
 
 
->>>>>>> Patch2
 
 
